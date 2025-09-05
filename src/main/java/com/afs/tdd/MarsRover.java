8152a251ebee129d58db3335fea9749595002dee
Danny Lau <lauda2@oocl.com>
2025-09-05 16:57:59 +0800

    feat: executeCommand backwards

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -38,6 +38,16 @@ public class MarsRover {
             } else if (location.direction() == Direction.W) {
                 location = new Location(location.x(), location.y(), Direction.N);
             }
+        } else if (command == Command.B) {
+            if (location.direction() == Direction.N) {
+                location = new Location(location.x(), location.y() - 1, Direction.N);
+            } else if (location.direction() == Direction.E) {
+                location = new Location(location.x() - 1, location.y(), Direction.E);
+            } else if (location.direction() == Direction.S) {
+                location = new Location(location.x(), location.y() + 1, Direction.S);
+            } else if (location.direction() == Direction.W) {
+                location = new Location(location.x() + 1, location.y(), Direction.W);
+            }
         }
     }
 
