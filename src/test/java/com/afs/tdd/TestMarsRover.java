e71a1522be3c0dc3c20526b5cb0f8282e479b2e2
Danny Lau <lauda2@oocl.com>
2025-09-05 16:23:20 +0800

    test: should_return_y_minus_when_command_M

--- src/test/java/com/afs/tdd/TestMarsRover.java
+++ src/test/java/com/afs/tdd/TestMarsRover.java
@@ -26,4 +26,15 @@ public class TestMarsRover {
         Location expectLocation = new Location(1, 0, Direction.E);
         assertEquals(marsRover.getLocation().toString(), expectLocation.toString());
     }
+
+    @Test
+    public void should_return_y_minus_when_command_M() {
+        Location location = new Location(0, 0, Direction.S);
+        MarsRover marsRover = new MarsRover(location);
+
+        marsRover.executeCommand(Command.M);
+
+        Location expectLocation = new Location(0, -1, Direction.S);
+        assertEquals(marsRover.getLocation().toString(), expectLocation.toString());
+    }
 }
