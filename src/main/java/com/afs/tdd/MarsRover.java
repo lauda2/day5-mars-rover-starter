39f7b3dcc1e59b8c0e83e7313d2c39ae341b389f
Danny Lau <lauda2@oocl.com>
2025-09-05 16:22:33 +0800

    feat: executeCommand

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -8,7 +8,15 @@ public class MarsRover {
     }
 
     public void executeCommand(Command command) {
-        location = new Location(0, 1, Direction.N);
+        if (location.direction() == Direction.N) {
+            location = new Location(0, 1, Direction.N);
+        } else if (location.direction() == Direction.E) {
+            location = new Location(1, 0, Direction.E);
+        } else if (location.direction() == Direction.S) {
+            location = new Location(0, -1, Direction.S);
+        } else if (location.direction() == Direction.W) {
+            location = new Location(-1, 0, Direction.W);
+        }
     }
 
     public Location getLocation() {
