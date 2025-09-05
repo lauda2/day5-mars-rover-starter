84a827c9e597fbbcb6a36ad86f41c41afd82b54e
Danny Lau <lauda2@oocl.com>
2025-09-05 16:27:20 +0800

    fix: executeCommand

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -8,14 +8,26 @@ public class MarsRover {
     }
 
     public void executeCommand(Command command) {
-        if (location.direction() == Direction.N) {
-            location = new Location(0, 1, Direction.N);
-        } else if (location.direction() == Direction.E) {
-            location = new Location(1, 0, Direction.E);
-        } else if (location.direction() == Direction.S) {
-            location = new Location(0, -1, Direction.S);
-        } else if (location.direction() == Direction.W) {
-            location = new Location(-1, 0, Direction.W);
+        if (command == Command.M) {
+            if (location.direction() == Direction.N) {
+                location = new Location(0, 1, Direction.N);
+            } else if (location.direction() == Direction.E) {
+                location = new Location(1, 0, Direction.E);
+            } else if (location.direction() == Direction.S) {
+                location = new Location(0, -1, Direction.S);
+            } else if (location.direction() == Direction.W) {
+                location = new Location(-1, 0, Direction.W);
+            }
+        } else if (command == Command.L) {
+            if (location.direction() == Direction.N) {
+                location = new Location(0, 0, Direction.W);
+            } else if (location.direction() == Direction.E) {
+                location = new Location(0, 0, Direction.N);
+            } else if (location.direction() == Direction.S) {
+                location = new Location(0, 0, Direction.E);
+            } else if (location.direction() == Direction.W) {
+                location = new Location(0, 0, Direction.S);
+            }
         }
     }
 
