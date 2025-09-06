00eae678ea18c01d36cb823a600dc86b73e9a903
Danny Lau <lauda2@oocl.com>
2025-09-06 13:08:06 +0800

    feat: add left right func in Direction

--- src/main/java/com/afs/tdd/Direction.java
+++ src/main/java/com/afs/tdd/Direction.java
@@ -1,3 +1,23 @@
 package com.afs.tdd;
 
-public enum Direction {N, E, S, W}
+public enum Direction {
+    N, E, S, W;
+
+    public Direction left() {
+        return switch (this) {
+            case N -> W;
+            case E -> N;
+            case S -> E;
+            case W -> S;
+        };
+    }
+
+    public Direction right() {
+        return switch (this) {
+            case N -> E;
+            case E -> S;
+            case S -> W;
+            case W -> N;
+        };
+    }
+}
