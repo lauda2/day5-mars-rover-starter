259431d61738a23d567b6d7822f9ed9be7b0f5d0
Danny Lau <lauda2@oocl.com>
2025-09-06 13:15:10 +0800

    refactor: simplify executeCommand by adding move()

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 public class MarsRover {
     private Location location;
+    private final int FORWARD = 1;
+    private final int BACKWARD = -1;
 
     public MarsRover(Location location) {
         this.location = location;
@@ -11,19 +13,19 @@ public class MarsRover {
 
     public void executeCommand(Command command) {
         switch (command) {
-            case M -> location = new Location(
-                    location.x() + (location.direction() == Direction.E ? 1 : location.direction() == Direction.W ? -1 : 0),
-                    location.y() + (location.direction() == Direction.N ? 1 : location.direction() == Direction.S ? -1 : 0),
-                    location.direction()
-            );
-            case B -> location = new Location(
-                    location.x() + (location.direction() == Direction.E ? -1 : location.direction() == Direction.W ? 1 : 0),
-                    location.y() + (location.direction() == Direction.N ? -1 : location.direction() == Direction.S ? 1 : 0),
-                    location.direction()
-            );
+            case M -> move(FORWARD);
+            case B -> move(BACKWARD);
             case L -> location = new Location(location.x(), location.y(), location.direction().left());
             case R -> location = new Location(location.x(), location.y(), location.direction().right());
         }
+    }
+
+    public void move(int movement) {
+        location = new Location(
+                location.x() + (location.direction() == Direction.E ? movement : location.direction() == Direction.W ? -movement : 0),
+                location.y() + (location.direction() == Direction.N ? movement : location.direction() == Direction.S ? -movement : 0),
+                location.direction()
+        );
     }
 
     public Location getLocation() {
